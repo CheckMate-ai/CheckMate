cc96c82c279a899a79ff6ba9ee3f19baf9acedd4
kamaiel <raph.francoise@orange.fr>
2024-02-25 12:44:05 +0100

    use custom search API

--- backend/server/src/main/kotlin/backend/server/ServerApplication.kt
+++ backend/server/src/main/kotlin/backend/server/ServerApplication.kt
@@ -1,25 +1,21 @@
 package backend.server
 
+import backend.server.googleAPI.GoogleAPI
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-import backend.server.googleAPI.GoogleAPI
-
-@SpringBootApplication
-class ServerApplication
+@SpringBootApplication class ServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<ServerApplication>(*args)
+    runApplication<ServerApplication>(*args)
 }
 
 @RestController
 class MessageController {
-    @GetMapping("/")
-    fun index(@RequestParam("name") name: String)= "Hello, $name!"
+    @GetMapping("/") fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
 
 @RestController
