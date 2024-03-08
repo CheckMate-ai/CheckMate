af0007575cd5ea99da60f89387a54b32ca0c51fa
Bibi210 <brahimadibassi@gmail.com>
2024-03-08 10:47:00 +0100

    AI

--- backend/server/src/main/kotlin/backend/server/ServerApplication.kt
+++ backend/server/src/main/kotlin/backend/server/ServerApplication.kt
@@ -2,11 +2,9 @@ package backend.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication class ServerApplication
+
 fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
 }
