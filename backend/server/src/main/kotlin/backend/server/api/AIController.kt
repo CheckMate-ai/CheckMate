177393386fe3a84bb88102bc1ee9eea9b7b1da53
Diez <62673882+zeidfazazi@users.noreply.github.com>
2024-03-08 11:15:19 +0100

    feat(checkmate): connect to the server [CM-72]

--- backend/server/src/main/kotlin/backend/server/api/AIController.kt
+++ backend/server/src/main/kotlin/backend/server/api/AIController.kt
@@ -3,12 +3,14 @@ package backend.server.api
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
 import backend.server.AI.KeyWordAI
 
 @RestController
 class AIController {
     val keywordAI = KeyWordAI()
 
+    @CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
     @GetMapping("/keyword")
     fun ask(@RequestParam question: String): String {
         return keywordAI.getKeyword(question).toString()
--- checkmate_front/src/components/Counter.svelte
+++ checkmate_front/src/components/Counter.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
   import { onDestroy, onMount } from "svelte";
 
-  export let count: number;
-  let message: string | null = null;
-  let information: string | null = null
+  const BASE_URL = "http://localhost:8080/"
 
-  const increment = () => (count += 1);
-  const decrement = () => (count -= 1);
+  let promise: Promise<string[]> = fetch_keywords()
 
-  onMount(async () => {
+  async function fetch_keywords() {
     const info  = await chrome.storage.local.get();
-    information = info.message
-  })
-</script>
+    const param = new URLSearchParams({
+      question: info.message
+    })
+    const req = await fetch(BASE_URL+ "keyword?" +param)
+    const data = await req.json()
+    return data["result"]
+  }
 
+</script>
 <div>
-  <p>
-    {information!= null ? "Texte sélectionné : " : "Vous n'avez rien sélectionné"}
-  </p>
-  <div>
-    {#if information}<span>{information}</span>{/if}
-  </div>
+  {#await promise}
+	<p>...waiting</p>
+  {:then keywords}
+  <ul>
+    {#each keywords as keyword}
+      <li>{keyword}</li>
+    {/each}
+  </ul>
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
 </div>
