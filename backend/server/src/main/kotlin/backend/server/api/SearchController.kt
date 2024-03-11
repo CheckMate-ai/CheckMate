f60fc618f8cb1a6ad88291c33cdddcdb4d21c402
kamaiel <raph.francoise@orange.fr>
2024-03-11 15:28:47 +0100

    finalize safe and unsafe search

--- backend/server/src/main/kotlin/backend/server/api/SearchController.kt
+++ backend/server/src/main/kotlin/backend/server/api/SearchController.kt
@@ -1,16 +1,16 @@
 package backend.server.api
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestParam
 import backend.server.core.GoogleAPI
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SearchController {
     val googleSearchService = GoogleAPI()
 
     @GetMapping("/search")
-    fun search(@RequestParam query: String): List<String> {
+    fun search(@RequestParam query: String): String {
         return googleSearchService.getGoogleLinks(query)
     }
 }
