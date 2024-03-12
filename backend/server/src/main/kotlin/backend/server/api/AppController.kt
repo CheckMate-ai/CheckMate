daac62ffe1a436b99ca48ce74fe5251d881ef69f
Dibassi Brahima <58879980+Bibi210@users.noreply.github.com>
2024-03-12 17:15:15 +0100

    Get Sources From Sentence (#10)

---
+++ backend/server/src/main/kotlin/backend/server/api/AppController.kt
@@ -0,0 +1,22 @@
+package backend.server.api
+
+import backend.server.AI.KeyWordAI
+import backend.server.core.GoogleAPI
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AppController {
+    val keywordAI = KeyWordAI()
+    val googleSearchService = GoogleAPI()
+
+    @GetMapping("/getSources")
+    fun ask(@RequestParam question: String): String {
+        val keywords = keywordAI.getKeyword(question)
+        val linksJson = googleSearchService.getSources(keywords)
+        linksJson.put("question", question)
+        linksJson.put("keywords", keywords)
+        return linksJson.toString()
+    }
+}
